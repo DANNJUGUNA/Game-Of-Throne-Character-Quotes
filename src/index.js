e077295963f04edbbb846805ab01670510f9c9b8
DANNJUGUNA <ndan30071@gmail.com>
2022-12-13 13:52:31 +0300

    displayed the list of all the characters of the game of throne

--- src/index.js
+++ src/index.js
@@ -7,8 +7,15 @@ const fetchData = () => {
   fetch("https://api.gameofthronesquotes.xyz/v1/characters")
     .then((response) => response.json())
     .then((data) =>{
-      data.forEach(element => {
-        listCharacters(element);
-      });
+      data.forEach(listCharacters);
     });
 };
+const listCharacters=(data)=>{
+  const div=document.querySelector("#characters");
+  let ul=document.createElement("ul");
+  let li=document.createElement('li');
+  li.innerHTML=data.name;
+  ul.appendChild(li);
+  div.appendChild(ul);
+}
+load();
