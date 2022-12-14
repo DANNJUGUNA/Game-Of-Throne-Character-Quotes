253e933c26c145b6940fb06dd998e72894609c2a
DANNJUGUNA <ndan30071@gmail.com>
2022-12-14 12:26:36 +0300

    added comments

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
-const load=()=>{
-  document.addEventListener('DOMContentLoaded',()=>{
+// Adding an event listener to the  dom
+const load = () => {
+  document.addEventListener("DOMContentLoaded", () => {
     fetchData();
-  })
-}
+  });
+};
+//fetching data  from the public API
 const fetchData = () => {
   fetch("https://api.gameofthronesquotes.xyz/v1/characters")
     .then((response) => response.json())
-    .then((data) =>{
+    .then((data) => {
       data.forEach(listCharacters);
     });
 };
-const listCharacters=(data)=>{
-  const div=document.querySelector("#characters");
-  let ul=document.createElement("ul");
-  let li=document.createElement('li');
-  li.innerHTML=data.name;
+//Displaying the characters names form the API
+const listCharacters = (data) => {
+  const div = document.querySelector("#characters");
+  let ul = document.createElement("ul");
+  let li = document.createElement("li");
+  li.innerHTML = data.name;
+  li.style.cursor = "pointer";
   ul.appendChild(li);
   div.appendChild(ul);
-}
+  li.addEventListener("click", () => {});
+};
 load();
